ee6bae9e348790ef5a705b530c20d81bd76ce98d
tked111999 <512686256@qq.com>
2019-02-24 17:33:30 +0800

    [code] 文件下载

--- imooc-security-demo/src/main/java/com/imooc/web/controller/FileController.java
+++ imooc-security-demo/src/main/java/com/imooc/web/controller/FileController.java
@@ -1,18 +1,22 @@
 package com.imooc.web.controller;
 
 import com.imooc.dto.FileInfo;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.Date;
 
 @RestController
 @RequestMapping("/file")
 public class FileController {
+
+    // 家里电脑路径
+    private final static String FOLDER_PATH = "C:\\Users\\Administrator\\IdeaProjects\\imooc-security\\imooc-security-demo\\src\\main\\resources";
 
     @PostMapping
     public FileInfo upload(MultipartFile file) throws IOException {
@@ -20,10 +24,25 @@ public class FileController {
         System.out.println(file.getOriginalFilename());
         System.out.println(file.getSize());
 
-        File localFile = new File("", new Date().getTime() + ".txt");
+        File localFile = new File(FOLDER_PATH, new Date().getTime() + ".txt");
 
         file.transferTo(localFile);
 
         return new FileInfo(localFile.getAbsolutePath());
     }
+
+    @GetMapping("/{id}")
+    public void download(@PathVariable String id, HttpServletRequest request, HttpServletResponse response) {
+        try (InputStream inputStream = new FileInputStream(new File(FOLDER_PATH, id + ".txt"));
+             OutputStream outputStream = response.getOutputStream()) {
+            response.setContentType("application/x-download");
+            response.setHeader("Content-Disposition", "attachment;filename=download.txt");
+
+            IOUtils.copy(inputStream, outputStream);
+            outputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
